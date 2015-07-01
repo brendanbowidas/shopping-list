8646e425447c2f36fad0d8e4476997fb136f925f
brendanbowidas <brendanbowidas@gmail.com>
2015-07-01 14:39:48 -0400

    added additional functionality

--- js/main.js
+++ js/main.js
@@ -1,35 +1,72 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+      $('.list').sortable();
     //Remove all list items//
-    $('.clear').click(function(){
-              $('.list-item').fadeOut();
+    $('.clear').click(function () {
+        $('.list-item').fadeOut();
     });
-        //remove single item
-    $('.list').on('click', '.remove', function() {
+
+    //remove single item
+    $('.list').on('click', '.remove', function () {
         $(this).closest('.list-item').fadeOut();
     });
 
-    $('.list').on('click', '.box', function() {
-        $(this).closest('.list-item').toggleClass('checked');
+    //check off item
+    $('.list').on('click', '.box', function () {
 
-             $(this).children(':first').removeClass();
-             $(this).children(':first').attr('class', 'fa fa-check-square-o');
+        $(this).closest('.list-item').addClass('checked', 'fade');
+        var checked = $('<i class="fa fa-check-square-o" id="boxChecked">');
 
-
-
-
-
-
-
+        $(this).find('.fa').remove();
+        $(this).append(checked);
     });
 
-    //Add new item to list
-    $('.submit').click(function() {
+
+    //uncheck item
+    $('.list').on('click', '.box-checked', function () {
+
+        $(this).closest('.list-item.checked').removeClass('checked', 'fade');
+        var unChecked = $('<i class="fa fa-square-o" id="boxUnchecked">');
+
+        $(this).find('.fa').remove();
+        $(this).append(unChecked);
+    });
+
+
+
+
+
+    //Add new item to list(button)
+    $('.submit').click(function () {
         var newItem = $('#item').val();
-        $('#list-display .list').append('<li class="list-item"><span class="box"><i class="fa fa-square-o" id="checkBox"></i></span>' + newItem + '<span class="remove"><i class="fa fa-times-circle"></i></span></li>');
+        if (newItem != '') {
+            $('#list-display .list').append('<li class="list-item"><span class="box"><i class="fa fa-square-o" id="boxEmpty"></i></span>' + newItem + '<span class="remove"><i class="fa fa-times-circle"></i></span></li>');
+        }
         $('#item').val('');
+        $('.counter').text('40');
+    });
+
+    //Add new item to list(enter key)
+    $(document).keydown(function (e) {
+        var newItem = $('#item').val();
+        if (e.which === 13 && newItem != '') {
+            var newItem = $('#item').val();
+            $('#list-display .list').append('<li class="list-item"><span class="box"><i class="fa fa-square-o" id="boxEmpty"></i></span>' + newItem + '<span class="remove"><i class="fa fa-times-circle"></i></span></li>');
+            $('#item').val('');
+        }
+    });
+
+//check character length
+    $('#item').keyup(function(){
+        var itemLength = $(this).val().length;
+        var charactersLeft = 40 - itemLength;
+        $('.counter').text(charactersLeft);
+        if(charactersLeft < 0) {
+            $('.submit').prop('disabled', 'disabled')
+            $('.counter').text('Exceeded character limit!');
+        }
 
     });
 
 
 });
-
